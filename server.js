3ee7a503d53fb0c31631cde689a1c20c4ef36063
Landon <landongernand@outlook.com>
2023-01-05 23:30:39 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'front-end', 'build')))
+
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'))
+});
+
+app.listen(port, function() {
+    console.log('server spinning on port ' + port);
+});
